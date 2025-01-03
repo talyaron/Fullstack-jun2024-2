a112658bd9053f5f6f41e45e8dd8ad1e28f04468
Tal <tal.yaron@gmail.com>
2025-01-03 10:02:34 +0200

    example

---
+++ 06-React/02-Router/example/src/view/pages/home/posts/Posts.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Posts.module.scss'
+
+const Posts = () => {
+  return (
+    <div className={styles.posts}>Posts</div>
+  )
+}
+
+export default Posts
---
+++ 06-React/02-Router/example/src/view/pages/home/profile/Profile.tsx
@@ -0,0 +1,9 @@
+
+import styles from './Profile.module.scss'
+const Profile = () => {
+  return (
+    <div className={styles.profile}>Profile</div>
+  )
+}
+
+export default Profile
