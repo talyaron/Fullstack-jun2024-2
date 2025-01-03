00f813b6677153e0cf16589ecab96f2aefcc2db1
Alexis <gog20@icloud.com>
2025-01-03 13:03:45 +0200

    av followup

--- 06-React/02-Router/followups/Alexis/src/pages/Contacts.tsx
+++ 06-React/02-Router/followups/Alexis/src/pages/Contacts.tsx
@@ -1,13 +1,18 @@
+import { Link, Outlet } from "react-router";
+import React from "react";
 
-
-const Contacts = () =>{
+const Contact = () => {
+  return (
     <div>
-        <ul>
-            <li>Instagram</li>
-            <li>Telegram</li>
-            <li>WhatsApp</li>
-        </ul>
+      <ul>
+        <li>Instagram</li>
+        <li>Telegram</li>
+        <li>WhatsApp</li>
+        <Link to="home">Home </Link>
+      </ul>
+      <Outlet />
     </div>
-}
+  );
+};
 
-export default Contacts;
+export default Contact;
