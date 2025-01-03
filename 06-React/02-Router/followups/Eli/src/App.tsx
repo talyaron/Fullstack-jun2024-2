42d1b58b201772c97a5470da9faf87d1c6cd6ef5
Elizigi <elizigi876@gmail.com>
2025-01-03 10:30:42 +0200

    Done

---
+++ 06-React/02-Router/followups/Eli/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link, NavLink, Outlet } from "react-router";
+
+function App() {
+  return (
+    <>
+      <h1>welcome  this site!</h1>
+      
+    </>
+  );
+}
+
+export default App;
