f48a92e266e78648f1f024923d26272e32185177
Galit97 <fitness.galit@gmail.com>
2025-01-05 18:09:02 +0200

    followup-finished-Galit

--- 06-React/02-Router/followups/Galit/vite-project/src/view/pages/home/post/Post.tsx
+++ 06-React/02-Router/followups/Galit/vite-project/src/view/pages/home/post/Post.tsx
@@ -1,28 +1,30 @@
-
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router'
+import { useParams } from 'react-router'; 
 
 const Post = () => {
+  const { id } = useParams(); 
+  const [imageUrl, setImageUrl] = useState<string>('');
 
-    const { id } = useParams();
-    const [imageUrl, setImageUrl] = useState<string>('')
+  useEffect(() => {
+    fetch(`https://api.thecatapi.com/v1/images/${id}`) 
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setImageUrl(data.url); 
+      })
+      .catch(err => console.error('Failed to fetch image:', err));
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`https://api.thecatapi.com/images/${id}.jpg`)
-        .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setImageUrl(data.message)
-            })
+  return (
+    <div>
+      <h1>post: {id}</h1>
+      {imageUrl ? (
+        <img src={imageUrl} alt={`Cat ${id}`} style={{ maxWidth: '30rem' }} />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
-    }, [id])
-
-    return (
-        <div>
-            <h1>post: {id}</h1>
-            <img src={imageUrl} alt={id} />
-        </div>
-    )
-}
-
-export default Post
+export default Post;
