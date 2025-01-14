8fcf66ea88f666c34b246d71e3362b1e43de0141
Yosef Ibrahim <yosefib88@gmail.com>
2025-01-15 00:13:50 +0200

    yosef-fix

--- 06-React/03-MVVM/class-puzzle-balloons/yosef/yosef/src/view/components/ballonGame/BallonGame.tsx
+++ 06-React/03-MVVM/class-puzzle-balloons/yosef/yosef/src/view/components/ballonGame/BallonGame.tsx
@@ -1,27 +1,28 @@
 import { ballonGameMV } from "./BallonGameMV";
 import { Ballon } from "../../../model/ballon/ballon";
-import { useEffect } from "react";
-
-var x:any = 0;
-
+import style from "./BallonGame.module.scss"
 
 const StartPlay = () => {
 
-    const { ballons , setBallons, createNewBallon , getBallonImageUrl} = ballonGameMV();
+    const { ballons , setBallons} = ballonGameMV();
 
-useEffect(() => {
-    createNewBallon();
-    // x=getBallonImageUrl();
-
-    }, []);
-
-            // createNewBallon();  
-            console.log(ballons)
-            console.log(x)
+    function createBallon(){
+        const newBallon: Ballon = {
+            id: "123",
+            imageUrl: "https://partysocial.ae/cdn/shop/files/premium-balloon-5in-13cm-15-per-pack-party-social-1_grande.webp?v=1720101498"
+            };
+            setBallons([...ballons, newBallon]);
+    
+    }
             
 
     return (<div>
-        <h1>hey{ballons.length} </h1>
+        <h1>hey, you create {ballons.length} ballons </h1>
+        <button onClick={createBallon}>lets play</button>
+        {ballons.map(ballon => 
+        <div className={style.ballons} key={ballon.id}>
+            <img src={ballon.imageUrl} alt={ballon.id} />
+        </div>)}
         </div>
     );
 }
