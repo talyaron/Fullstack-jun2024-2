23206c30ef9fa0f2881fd63b3ba1e24c2e115825
Tal Yaron <tal.yaron@gmail.com>
2025-01-14 19:01:57 +0200

    MVVM - puzzle

--- 06-React/03-MVVM/example-todo/src/App.tsx
+++ 06-React/03-MVVM/example-todo/src/App.tsx
@@ -1,15 +1,10 @@
-
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
-import { NavLink } from 'react-router'
-import { useApp } from './AppVM'
-
+import './App.css'
 
 function App() {
-  const { count, setCount } = useApp()
-//const [count, setCount] = useState(0) //ViewModel
-
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -21,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + 55 React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +25,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <NavLink to="/about">About</NavLink>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- 06-React/03-MVVM/example/src/App.tsx
+++ 06-React/03-MVVM/example/src/App.tsx
@@ -1,27 +1,20 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import { NavLink } from 'react-router'
+import './App.css'
 import { useApp } from './AppVM'
 
-
 function App() {
-  const { count, setCount } = useApp()
-//const [count, setCount] = useState(0) //ViewModel
-
+  const {count, setCount,dog} = useApp(); //ViewModel
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          {dog.message && <img src={dog.message} className="logo" alt="Vite logo" />}
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {dog.message && <img src={dog.message} className="logo react" alt="React logo" />}
         </a>
       </div>
-      <h1>Vite + 55 React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,7 +23,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <NavLink to="/about">About</NavLink>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
