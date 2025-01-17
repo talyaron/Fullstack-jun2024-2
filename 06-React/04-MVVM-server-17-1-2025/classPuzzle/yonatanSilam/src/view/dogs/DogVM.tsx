2f21d967577c4ddf9675a68e012c59c2cb26240d
Tal <tal.yaron@gmail.com>
2025-01-17 12:40:06 +0200

    Home puzzle

--- 06-React/04-MVVM-server-17-1-2025/classPuzzle/yonatanSilam/src/view/dogs/DogVM.tsx
+++ 06-React/04-MVVM-server-17-1-2025/classPuzzle/yonatanSilam/src/view/dogs/DogVM.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { ThreeDogs } from "./DogModel";
+import { DogsModel } from "./DogModel";
 
 export function useDogsVM() {
-  const [dogs, setDogs] = useState<ThreeDogs | null>(null);
+  const [dogs, setDogs] = useState<DogsModel | null>(null);
   useEffect(() => {
-    fetch("https://dog.ceo/api/breed/hound/images/random/3")
+    fetch("https://dog.ceo/api/breed/hound/images/random/6")
       .then((response) => response.json())
       .then((data) => setDogs(data))
       .catch((error) => console.error(error));
---
+++ 06-React/04-MVVM-server-17-1-2025/homePuzzle/instructions.md
@@ -0,0 +1,6 @@
+Level 1: try to create the behavior that will be shown on the next video
+  
+  * create pseudo code for the behavior (60 points)
+  * If posable create the behavior in React (20 points)
+  
+  Level 2: Solve the drag problem. Write pseudo code and math calculation to let the rectangle stay in the place it was dropped. (20 points)
