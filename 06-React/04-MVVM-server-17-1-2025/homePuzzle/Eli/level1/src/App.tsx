3daf89d2381322c203a166ee3b809c163f71a474
Elizigi <elizigi876@gmail.com>
2025-01-18 20:55:51 +0200

    level2 finished

--- 06-React/04-MVVM-server-17-1-2025/homePuzzle/Eli/level1/src/App.tsx
+++ 06-React/04-MVVM-server-17-1-2025/homePuzzle/Eli/level1/src/App.tsx
@@ -7,9 +7,9 @@ import "./App.css";
   2.iterate through the array of objects and give css styling of an active button if its active
 
   3.when a button is clicked iterate throughout the array deactivate every other button that isnt the one 
-  we clicked by setting active to false and the one that we clicked 
+  we clicked by setting active to false turning them off
 
-  4.revere its active state so if its on it would turn off and if its off 
+  4.change the button we clicked to the state it isnt so if its on it would turn off and if its off 
   it would turn on 
   
   5. check if the clicked button is active and if it is set the background color to its color 
@@ -32,13 +32,16 @@ const allButtons: Btn[] = [
 
 const defaultColor = "white";
 
+const selectedCssClass ="selected btn btnInner";
+const unselectedCssClass ="unSelected btn btnInner"
+
 function App() {
   const [buttons, setButtons] = useState<Btn[]>(allButtons);
 
   const [backgroundColor, setColor] = useState(defaultColor);
 
   function change(i: number) {
-    setColor(buttons[i].active ?defaultColor : buttons[i].color );
+    setColor(buttons[i].active ? defaultColor : buttons[i].color);
     setButtons(
       buttons.map((btn, idx) => ({
         ...btn,
@@ -50,17 +53,15 @@ function App() {
   return (
     <>
       <div className="page" style={{ backgroundColor }}>
-        {buttons.map((btn, i) =>
-          btn.active ? (
-            <div key={i} className="btnContainer btn" onClick={() => change(i)}>
-              <div className="btnInner selected btn"></div>
-            </div>
-          ) : (
-            <div key={i} className="btnContainer" onClick={() => change(i)}>
-              <div className="btnInner unSelected"></div>
-            </div>
-          )
-        )}
+        {buttons.map((btn, i) => (
+          <div key={i} className="btnContainer btn" onClick={() => change(i)}>
+            <div
+              className={
+                btn.active ? selectedCssClass: unselectedCssClass
+              }
+            ></div>
+          </div>
+        ))}
       </div>
     </>
   );
