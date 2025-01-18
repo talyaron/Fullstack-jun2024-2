6b1d50b7a0d35601297331d4e46887194e178807
Elizigi <elizigi876@gmail.com>
2025-01-18 02:24:57 +0200

    P1 DONE

--- 06-React/04-MVVM-server-17-1-2025/homePuzzle/Eli/src/App.tsx
+++ 06-React/04-MVVM-server-17-1-2025/homePuzzle/Eli/src/App.tsx
@@ -6,16 +6,44 @@ interface Btn {
   color: string;
   active: boolean;
 }
+/*1.make an array of objects to represent the button with (i went with active and color) 
+
+  2.iterate through the array of objects and give css styling of an active button if its active
+
+  3.when a button is clicked iterate throughout the array deactivate every other button that isnt the one 
+  we clicked by setting active to false and the one that we clicked 
+
+  4.revere its active state so if its on it would turn off and if its off 
+  it would turn on 
+  
+  5. check if the clicked button is active and if it is set the background color to its color 
+  if its not change it back to white
+*/
+
+
 function App() {
-  const [buttons, setButtons] = useState<boolean[]>([true, false]);
+  const [buttons, setButtons] = useState<Btn[]>([
+    { color: "blue", active: false },
+    { color: "yellow", active: false },
+  ]);
+
+  const [backgroundColor ,setColor]=useState("white");
 
   function change(i: number) {
-    setButtons(buttons.map((btn, idx) => (i === idx ? !btn : false)));
+    setColor(!buttons[i].active? buttons[i].color:"white");
+    setButtons(
+      buttons.map((btn, idx) => ({
+        ...btn,
+        active: i === idx ? !btn.active : false,
+      }))
+    );
   }
+
   return (
     <>
+        <div style={{ backgroundColor }}>
       {buttons.map((btn, i) =>
-        btn ? (
+        btn.active ? (
           <div key={i} className="btnContainer btn" onClick={() => change(i)}>
             <div className="btnInner selected btn"></div>
           </div>
@@ -25,6 +53,7 @@ function App() {
           </div>
         )
       )}
+      </div>
     </>
   );
 }
