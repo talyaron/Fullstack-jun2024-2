1e8740c40e5aceb4a04cc5667a703395d637b77b
Yosef Ibrahim <yosefib88@gmail.com>
2025-01-18 13:26:13 +0200

    fix ballon game

--- 06-React/03-MVVM/class-puzzle-balloons/yosef/yosef/src/view/components/ballonGame/BallonGame.tsx
+++ 06-React/03-MVVM/class-puzzle-balloons/yosef/yosef/src/view/components/ballonGame/BallonGame.tsx
@@ -1,67 +1,73 @@
+import { useEffect, useState } from "react";
 import { ballonGameMV } from "./BallonGameMV";
 import { Ballon } from "../../../model/ballon/ballon";
-import style from "./BallonGame.module.scss"
-import ballonPic from '../../../pics/ballon.png'
-import ballonExplode from '../../../pics/explode.png'
-import { useEffect } from "react";
-
-
+import style from "./BallonGame.module.scss";
+import ballonPic from '../../../pics/ballon.png';
+import ballonExplode from '../../../pics/explode.png';
 
 const StartPlay = () => {
+    const { ballons, setBallons, score, setScore, timer, setTimer } = ballonGameMV();
+    const [isPlaying, setIsPlaying] = useState(false);
 
-    const { ballons , setBallons, score, setScore, timer, setTimer} = ballonGameMV();
+    useEffect(() => {
+        if (isPlaying) {
+            const timerInterval = setInterval(() => {
+                setTimer(prevTimer => prevTimer + 0.5);
+            }, 1000);
 
-    function createBallon(){
-        const leftMove = Math.floor(Math.random() *1000);
-        const topMove = Math.floor(Math.random() *1000);
+            const ballonInterval = setInterval(createBallon, 1500);
+
+            return () => {
+                clearInterval(timerInterval);
+                clearInterval(ballonInterval);
+            };
+        }
+    }, [isPlaying, setTimer, setBallons]);
+
+    function createBallon() {
+        const leftMove = Math.floor(Math.random() * 1000);
+        const topMove = Math.floor(Math.random() * 1000);
         const newBallon: Ballon = {
-            id: Math.floor(Math.random() *1000).toString(),
+            id: Math.floor(Math.random() * 1000).toString(),
             imageUrl: ballonPic,
             left: leftMove,
             top: topMove
-            };
-            setBallons([...ballons, newBallon]);
-            
+        };
+        setBallons([...ballons, newBallon]);
     }
 
     function popTheBallon(currentBallon: string) {
-        setBallons(ballons.map(b => 
-            b.id === currentBallon 
-            ? { ...b, imageUrl: ballonExplode } 
-            : b
+        setBallons(ballons.map(b =>
+            b.id === currentBallon
+                ? { ...b, imageUrl: ballonExplode }
+                : b
         ));
     }
-    
-function start(){
-    useEffect(() => {   
-        const timerInterval = setInterval(() => {
-            setTimer(prevTimer => prevTimer+0.5);
-        }, 1000);
-        const interval = setInterval(createBallon, 1500);
-        return () => clearInterval(interval);
-    }, [setBallons]);
-    // setInterval(createBallon, 3000);
-}
-            
 
-    return (<div>
-        <p className={style.timer}>Timer: {timer}</p>
-        <p className={style.score}>Score: {score}</p>
-        <button className={style.play} onClick={start()}>Lets Play</button>
-        {ballons.map(ballon => 
-        <div className={style.ballons} key={ballon.id}>
-            <img 
-            src={ballon.imageUrl}
-            alt={ballon.id}
-            style={{position:"absolute", left:ballon.left, top:ballon.top}}
-            onClick={() => {
-                popTheBallon(ballon.id)
-                setScore(score + 1);
-            }}
-            />
-        </div>)}
+    function start() {
+        setIsPlaying(true);
+    }
+
+    return (
+        <div>
+            <p className={style.timer}>Timer: {timer}</p>
+            <p className={style.score}>Score: {score}</p>
+            <button className={style.play} onClick={start}>Lets Play</button>
+            {ballons.map(ballon =>
+                <div className={style.ballons} key={ballon.id}>
+                    <img
+                        src={ballon.imageUrl}
+                        alt={ballon.id}
+                        style={{ position: "absolute", left: ballon.left, top: ballon.top }}
+                        onClick={() => {
+                            popTheBallon(ballon.id);
+                            setScore(score + 1);
+                        }}
+                    />
+                </div>
+            )}
         </div>
     );
 }
 
-export default StartPlay
+export default StartPlay;
