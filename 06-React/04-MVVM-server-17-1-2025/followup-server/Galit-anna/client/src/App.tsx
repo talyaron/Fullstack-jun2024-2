0d90876e1d5e8ac03f73845f542f63c16f25d9c0
Galit97 <fitness.galit@gmail.com>
2025-01-21 20:37:16 +0200

    class-puzlle galit anna

---
+++ 06-React/04-MVVM-server-17-1-2025/followup-server/Galit-anna/client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Form from './view/component/form/form'
+import Product from './view/component/products/Product'
+
+function App() {
+
+
+  return (
+    <>
+    <Form />
+      <Product />
+    </>
+  )
+}
+
+export default App
