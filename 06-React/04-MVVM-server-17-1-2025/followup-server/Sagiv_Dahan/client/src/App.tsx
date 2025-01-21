b61f3556c7ae3f1082877541f40ee90051c12bc7
Sagiv Dahan <adhcsvi1@gmail.com>
2025-01-21 20:28:21 +0200

    Done

---
+++ 06-React/04-MVVM-server-17-1-2025/followup-server/Sagiv_Dahan/client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddProduct from './view/component/addProduct/AddProduct'
+import Product from './view/component/products/Product'
+
+function App() {
+
+
+  return (
+    <>
+      <AddProduct/>
+      <Product />
+    </>
+  )
+}
+
+export default App
