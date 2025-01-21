6af5113e125c0d06e1d998bfa45c53561348495b
Yulia <u.kaganovich@gmail.com>
2025-01-21 20:09:08 +0200

    upd

--- 06-React/04-MVVM-server-17-1-2025/followup-server/Yulia/client/src/view/component/products/ProductVM.tsx
+++ 06-React/04-MVVM-server-17-1-2025/followup-server/Yulia/client/src/view/component/products/ProductVM.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { Product } from "../../../model/productModel";
 
 
 export function useProductsVM() {
@@ -13,5 +15,11 @@ export function useProductsVM() {
 async function fetchProducts(): Promise<Product[]> {
     try {
         const res = await fetch('http://localhost:3000/api/products/get-all-products');
-
+        const data = await res.json();
+        if (!data.products) throw new Error('No products found');
+        return data.products;
+    } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
