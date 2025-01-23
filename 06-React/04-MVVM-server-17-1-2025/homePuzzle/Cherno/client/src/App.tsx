7b8e3b7ba255d74e0a153faf5bfb1445d1e7926f
Cherno <daniel.cherno9@gmail.com>
2025-01-23 23:52:45 +0200

    homepuzzle

---
+++ 06-React/04-MVVM-server-17-1-2025/homePuzzle/Cherno/client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Puzzle from './components/SwitchContainer'
+
+function App() {
+  //const {count, setCount} = useSwitchVm();
+
+  return (
+    <>
+      <Puzzle></Puzzle>
+    </>
+  )
+}
+
+export default App
