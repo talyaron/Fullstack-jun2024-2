4682f3ab5353220a8fadc9d14096b115d3651cac
Yosef Ibrahim <yosefib88@gmail.com>
2025-01-25 00:38:33 +0200

    homepuzzle - server

---
+++ 06-React/04-MVVM-server-17-1-2025/home-puzzle-server/yosef2/server/src/server.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import mongoose from "mongoose";
+import cookieParser from 'cookie-parser';  
+import 'dotenv/config';
+const app = express()
+const port = 3000;
+
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//DB
+const dbUrl = process.env.DB_URL;
+const database = 'ToDoReactServer';
+
+//connection
+mongoose.connect(`${dbUrl}/${database}`).then(()=>{
+    console.info("DB connected")
+}).catch((err)=>{
+    console.error(err)
+});
+
+//routes
+import clientsRouter from './routes/clients/clientRoutes';
+app.use("/api/clients", clientsRouter);
+import productsRouter from './routes/products/productRoute';
+app.use("/api/products", productsRouter);
+import commentsRouter from './routes/comments/commentsRoute';
+app.use("/api/comments", commentsRouter);
+import Purchase from "./routes/purchase/purchaseRouter";
+app.use("/api/purchase", Purchase);
+import toDoRouter from './routes/toDo/toDoRoutes'
+app.use("/api/toDo", toDoRouter);
+
+app.listen(port, () => {
+  console.log(`To Do React Server is running on port ${port}`)
+})
