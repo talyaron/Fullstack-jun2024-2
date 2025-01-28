6bd5e31639f14e8e635592650e73c77490d7212a
Yosef Ibrahim <yosefib88@gmail.com>
2025-01-28 19:18:17 +0200

    class puzzle - update - server

--- 06-React/04-MVVM-server-17-1-2025/home-puzzle-server/yosef2/server/src/routes/toDo/toDoRoutes.ts
+++ 06-React/04-MVVM-server-17-1-2025/home-puzzle-server/yosef2/server/src/routes/toDo/toDoRoutes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { setToDo } from '../../controllers/toDo/setTitle';
 import { getToDos } from '../../controllers/toDo/getTitle';
+import { isDone } from '../../controllers/toDo/isDoneTitle';
+
 
 const router = express.Router();
 
 router.post('/set-title', setToDo);
 router.get('/get-title', getToDos);
+router.post('/is-done', isDone);
 
 
 export default router;
