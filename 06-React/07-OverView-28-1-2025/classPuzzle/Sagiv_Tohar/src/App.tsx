eab8d204ae81099da63f1f62baf3ad7e398a5ea2
Sagiv Dahan <adhcsvi1@gmail.com>
2025-01-28 20:53:19 +0200

    Done With Tic Tac Toe

---
+++ 06-React/07-OverView-28-1-2025/classPuzzle/Sagiv_Tohar/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Grid from './components/grid/Grid'
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Grid></Grid>
+    </>
+
+  )
+}
+
+export default App
