dd43fed78d16e7d80225f1b4f866a0e3f00011f3
silam50 <ysilam50@gmail.com>
2025-01-30 22:47:50 +0200

    d

--- 06-React/07-OverView-28-1-2025/classPuzzle/yonatanSilam/src/view/components/Cell/Cell.tsx
+++ 06-React/07-OverView-28-1-2025/classPuzzle/yonatanSilam/src/view/components/Cell/Cell.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { CellModel } from "../../../model/cell/CellModel";
 import style from "./Cell.module.scss";
+import { useTicTacToeVM } from "../../pages/ticTacToe/ticTacToeVM";
 
 interface Props {
   cell: CellModel;
   play: (cellId: number) => void;
 }
-<<<<<<< Updated upstream
+
 const Cell: React.FC<Props> = ({ cell, play }) => {
-  console.log(cell);
-
- 
-=======
-const { play } = useTicTacToeVM();
-const Cell: React.FC<Props> = ({ cell }) => {
-
->>>>>>> Stashed changes
   return (
-    <button onClick={()=>play(cell._id)} className={style.cell}>
+    <button onClick={() => play(cell._id)} className={style.cell}>
       <div>{cell.XorY}</div>
     </button>
   );
