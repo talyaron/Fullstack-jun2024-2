3b0747900bc3ef6e61862b454a126ca3d76da455
Yosef Ibrahim <yosefib88@gmail.com>
2025-02-03 22:52:31 +0200

    x / o game - done

--- 06-React/07-OverView-28-1-2025/classPuzzle/yosef/src/App.tsx
+++ 06-React/07-OverView-28-1-2025/classPuzzle/yosef/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
       <div>
-              <h1>Etimad</h1>
+              <h1 style={{backgroundColor:"yellow"}}>X / O Game</h1>
               <TicTacToe />
 
       </div>
