890f84177e90943df674388b4f9d9a4bec9d4c41
Tal <tal.yaron@gmail.com>
2025-02-07 12:50:28 +0200

    Home puzzle

--- 06-React/10-lifiting-state-up-07-02-2025/exmple/src/App.tsx
+++ 06-React/10-lifiting-state-up-07-02-2025/exmple/src/App.tsx
@@ -8,18 +8,16 @@ import Image from './view/components/image/Image'
 function App() {
   const [theme, setTheme] = useState<"dark" | "light">("light")
 
+  function handleSetState() {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
+
   return (
 
     <div className={`${theme} app`}>
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
-      <Box width={200} height={200} theme={theme} setTheme={setTheme} />
+      <Box width={200} height={200} theme={theme} setTheme={setTheme} handleSetState={handleSetState} />
+      <Box width={200} height={200} theme={theme} setTheme={setTheme} handleSetState={handleSetState} />
+     
       <Image src={reactLogo} alt="React logo" width={200} height={200} theme={theme} />
     </div>
 
