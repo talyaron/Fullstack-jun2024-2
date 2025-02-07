421fd6aefff034aa93a696d4ab9725c29cdc02b9
Sagiv Dahan <adhcsvi1@gmail.com>
2025-02-07 11:34:50 +0200

    Fix

--- 06-React/10-lifiting-state-up-07-02-2025/exmple/classPuzzle/noam-sagiv/src/App.tsx
+++ 06-React/10-lifiting-state-up-07-02-2025/exmple/classPuzzle/noam-sagiv/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import MainImage from './view/components/MainImage/MainImage'
 
 function App() {
-  const [image, setImage] = useState<"dog" | "dog2" | "dog3">()
+  // const [image, setImage] = useState<"dog" | "dog2" | "dog3">("dog")
 
   return (
     <>
+      <MainImage image={`URL`} alt="Cute Dog" />
     </>
   )
 }
