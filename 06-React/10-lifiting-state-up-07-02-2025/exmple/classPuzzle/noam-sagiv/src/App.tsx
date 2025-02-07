dfb41d3b1e982dbb3a99b5241c487e82561b9001
Sagiv Dahan <adhcsvi1@gmail.com>
2025-02-07 11:47:17 +0200

    Revert "Fix"
    This reverts commit 421fd6aefff034aa93a696d4ab9725c29cdc02b9.

--- 06-React/10-lifiting-state-up-07-02-2025/exmple/classPuzzle/noam-sagiv/src/App.tsx
+++ 06-React/10-lifiting-state-up-07-02-2025/exmple/classPuzzle/noam-sagiv/src/App.tsx
@@ -1,13 +1,12 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import MainImage from './view/components/MainImage/MainImage'
 
 function App() {
-  // const [image, setImage] = useState<"dog" | "dog2" | "dog3">("dog")
+  const [image, setImage] = useState<"dog" | "dog2" | "dog3">()
 
   return (
     <>
-      <MainImage image={`URL`} alt="Cute Dog" />
     </>
   )
 }
