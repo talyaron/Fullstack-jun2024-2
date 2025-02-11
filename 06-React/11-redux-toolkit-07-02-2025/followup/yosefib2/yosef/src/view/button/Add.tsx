54e303071af360bfc3dc0277cd94b82618338aa6
Yosef Ibrahim <yosefib88@gmail.com>
2025-02-11 19:17:02 +0200

    yosef redux follow up

---
+++ 06-React/11-redux-toolkit-07-02-2025/followup/yosefib2/yosef/src/view/button/Add.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { increment } from '../../redux/counter/CounterSlice'
+import { useDispatch } from 'react-redux'
+
+const AddButton = () =>{
+    const dispatch = useDispatch()
+
+    function handelAdd(){
+        dispatch(increment())
+    }
+
+
+  return (
+<button onClick={handelAdd}>
+    Add
+</button>
+  )
+}
+
+
+export default AddButton
