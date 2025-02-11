2336311c8f6b91ec3112aa48b2d3061d188e6d79
Alexis <gog20@icloud.com>
2025-02-11 19:05:53 +0200

    class puzzle alexis

--- 06-React/11-redux-toolkit-07-02-2025/followup/Alexis/src/main.tsx
+++ 06-React/11-redux-toolkit-07-02-2025/followup/Alexis/src/main.tsx
@@ -1,22 +1,14 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import { store } from './redux/store.ts'
+import App from './App.tsx'
 import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
-const container = document.getElementById('root')
-
-if (container) {
-  const root = createRoot(container)
-
-  root.render(
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
-}
+    </Provider>
+  </StrictMode>,
+)
