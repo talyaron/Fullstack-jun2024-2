c6d653fbf3af4d09b9425ba2e47b748f34079a88
silam50 <ysilam50@gmail.com>
2025-02-12 07:50:57 +0200

    ys

--- 06-React/07-OverView-28-1-2025/classPuzzle/yonatanSilam/src/view/components/Cell/Cell.tsx
+++ 06-React/07-OverView-28-1-2025/classPuzzle/yonatanSilam/src/view/components/Cell/Cell.tsx
@@ -7,8 +7,14 @@ interface Props {
   cell: CellModel;
   play: (cellId: number) => void;
 }
+<<<<<<< Updated upstream
 
 const Cell: React.FC<Props> = ({ cell, play }) => {
+=======
+const { play } = useTicTacToeVM();
+const Cell: React.FC<Props> = ({ cell }) => {
+
+>>>>>>> Stashed changes
   return (
     <button onClick={() => play(cell._id)} className={style.cell}>
       <div>{cell.XorY}</div>
