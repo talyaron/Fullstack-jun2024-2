04730c2b810bf07e9bee4bd7ff1e4abfd9173cc9
Tal Yaron <tal.yaron@gmail.com>
2025-04-04 09:57:57 +0300

    work thorugh

--- 08-SQL/02-join/walkTrhough/living.sql
+++ 08-SQL/02-join/walkTrhough/living.sql
@@ -1,11 +1,13 @@
-create table living (
-living_id int AUTO_INCREMENT,
-user_id int not null,
-city varchar(50) not null,
-street varchar(50) not null,
-house_number smallint unsigned not null,
-floor_number smallint,
-apprtment_number smallint unsigned,
-PRIMARY KEY (living_id),
-FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE living (
+    living_id INT AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    street VARCHAR(50) NOT NULL,
+    house_number SMALLINT UNSIGNED NOT NULL,
+    floor_number SMALLINT,
+    apprtment_number SMALLINT UNSIGNED,
+    PRIMARY KEY (living_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
 )
+
--- 08-SQL/02-join/walkTrhough/tal/living.sql
+++ 08-SQL/02-join/walkTrhough/tal/living.sql
@@ -1,2 +1,13 @@
-create table living (
-living_id int
+CREATE TABLE living (
+    living_id INT AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    street VARCHAR(50) NOT NULL,
+    house_number SMALLINT UNSIGNED NOT NULL,
+    floor_number SMALLINT,
+    apprtment_number SMALLINT UNSIGNED,
+    PRIMARY KEY (living_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
+)
+
