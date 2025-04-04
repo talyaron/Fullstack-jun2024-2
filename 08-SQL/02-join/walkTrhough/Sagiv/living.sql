78ead076bfc45c50b2cb69691e531461a11a9fb5
Sagiv Dahan <adhcsvi1@gmail.com>
2025-04-04 10:10:35 +0300

    Update living.sql

--- 08-SQL/02-join/walkTrhough/Sagiv/living.sql
+++ 08-SQL/02-join/walkTrhough/Sagiv/living.sql
@@ -4,10 +4,11 @@ USE sagiv_living;
 
 create table living(
 living_id int primary key,
+user_id INT,
 FOREIGN KEY (user_id) REFERENCES users(user_id) ,
 city varchar (50) not null,
 street varchar(50) not null,
 house_number SMALLINT,
 floor_number SMALLINT,
-floor_number SMALLINT,
+apartment_number SMALLINT
 );
