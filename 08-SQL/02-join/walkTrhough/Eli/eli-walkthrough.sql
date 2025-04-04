6e5bb8e6e74ebc0cbb9ff4388a01339656761ef7
Elizigi <elizigi876@gmail.com>
2025-04-04 10:08:11 +0300

    saved UNDO information for reset changes and discarded files

---
+++ 08-SQL/02-join/walkTrhough/Eli/eli-walkthrough.sql
@@ -0,0 +1,7 @@
+- living_id (int, primary key)
+ - user_id (int, foreign key to users table)
+ - city (varchar(50)) not null
+ - street (varchar(50)) not null
+ - house_number (int)
+  floor_number (int)
+ - apartment_number (int)
