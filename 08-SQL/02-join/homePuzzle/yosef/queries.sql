351a7e85d1177a5024fac09406df55864e23cc03
Yosef Ibrahim <yosefib88@gmail.com>
2025-04-08 03:10:48 +0300

    home puzzle done

--- 08-SQL/02-join/homePuzzle/yosef/queries.sql
+++ 08-SQL/02-join/homePuzzle/yosef/queries.sql
@@ -30,17 +30,75 @@
     
 --------------------------------------------   תציג לי את כל העובדים במחלקת הגרפיקה
     
-    SELECT 
-    e.first_name,
-    e.last_name,
-    e.phone,
-    d.description AS department_description,
-    p.description AS position_description
+--     SELECT 
+--     e.first_name,
+--     e.last_name,
+--     e.phone,
+--     d.description AS department_description,
+--     p.description AS position_description
+-- FROM
+--     employees e
+-- JOIN
+--     departments d ON e.department_id = d.department_id
+-- JOIN
+--     positions p ON e.position_id = p.position_id
+-- WHERE
+--     p.position_id = 3;
+
+-- תציג לי את כל ההזמנות כולל שם לקוח, מידות הסכין, ומס' גלופה שמשתמשים
+SELECT
+    c.customer_name,
+    o.order_date,
+    o.delivery_date,
+    o.praises,
+    k.knives_dimensions,
+    e.engravings_name
 FROM
-    employees e
-JOIN
-    departments d ON e.department_id = d.department_id
-JOIN
-    positions p ON e.position_id = p.position_id
-WHERE
-    p.position_id = 3
+    orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN knives k ON o.knives_id = k.knives_id
+JOIN engravings e ON e.engravings_id = o.engravings_id;
+
+-- תציג לי שם לקוח וכמה הזמנות יש לו
+SELECT
+    c.customer_name,
+    count(o.customer_id) as count_orders
+FROM
+    orders o
+JOIN customers c ON o.customer_id = c.customer_id
+group by o.customer_id
+having count(o.customer_id);
+
+-- תציג לי לקוחות שיש להם יותר מהזמנה אחת באותו יום
+SELECT
+	c.customer_name,
+    o.order_date,
+    count(o.order_id) as order_count
+FROM
+	orders o
+    JOIN customers c ON o.customer_id = c.customer_id
+group by
+    o.order_date, c.customer_name
+having count(o.order_date)>1;
+
+-- תציג לי ממוצע המשכורות בש.ל.ר
+select 
+avg(e.salary) as ממוצע_המשכורות_בשלר_הוא
+from 
+	employees e;
+
+-- תציג לי שם פרטי ומשפחה לעובד הכי וותיק בשלר
+SELECT 
+    first_name,last_name,
+    start_date AS תאריך_התחלת_עבודה
+FROM 
+    employees 
+ORDER BY 
+    start_date ASC
+LIMIT 1;
+
+    
+
+
+
+    
--- 08-SQL/02-join/homePuzzle/yosef/tables.sql
+++ 08-SQL/02-join/homePuzzle/yosef/tables.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS SLR_DataBase;
 use slr_database;
 
+-- טבלת עובדים
 create table Employeesdepartments(
 employees_id INT NOT NULL auto_increment primary key,
 first_name varchar(50) not null,
@@ -17,23 +18,82 @@ department_id int not null
 
 select * from employees;
 
+-- קשרתי את המזהה מחלקה לטבלת מחלקות
 alter table employees
 add constraint fk_department_id
 foreign key (department_id)
 references departments(department_id)
 on delete cascade;
 
+-- תפקידים בעבודה
 create table positions (
 position_id int not null auto_increment primary key,
 description varchar(50) not null
 );
 
+-- מחלקות בעבודה
 create table departments (
 department_id int not null auto_increment primary key,
 description varchar(50) not null
 );
 
+-- טבלת לקוחות
+create table customers (
+customer_id int not null auto_increment primary key,
+customer_name varchar(200),
+entry_date date);
+
+-- טבלת חומרי גלם
+create table materials (
+materials_id int not null auto_increment primary key,
+material_description varchar(200),
+material_width int,
+material_pet boolean
+);
+
+-- טבלת סכינים
+create table knives (
+knives_id int not null auto_increment primary key,
+knives_dimensions varchar(50),
+materials_id int,
+foreign key(materials_id) references materials(materials_id) on delete cascade,
+in_stock boolean,
+need_to_order boolean,
+sent_for_repair boolean
+);
+
+-- טבלת גלופות
+create table engravings (
+engravings_id int not null auto_increment primary key,
+engravings_name varchar(50) unique not null,
+engravings_description varchar(100)
+);
+
+-- טבלת הזמנות
+create table orders (
+order_id int not null auto_increment primary key,
+customer_id int,
+foreign key(customer_id) references customers(customer_id) on delete cascade,
+order_date date not null,
+delivery_date date not null,
+praises enum('Gloss varnish', 'Matte varnish', 'Matte lamination', 'Glossy lamination' ,
+'Embossing and debossing', 'Foil stampin only', 'Silk screen'),
+knives_id int,
+foreign key(knives_id) references knives(knives_id) on delete cascade,
+engravings_id int,
+foreign key(engravings_id) references engravings(engravings_id) on delete cascade
+);
 
 
 
+
+
+
+
+
+
+
+
+
+ 
 
