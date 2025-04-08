a7bb8f99f1fdc56413a51b836d739ab381569194
Elizigi <elizigi876@gmail.com>
2025-04-08 18:04:01 +0300

    a

--- 08-SQL/03-server/smallPuzzle/Eli/Eli.sql
+++ 08-SQL/03-server/smallPuzzle/Eli/Eli.sql
@@ -4,9 +4,9 @@ use eli_ecommerce;
 
 
 create table users(
-    user_id int AUTO_INCREMENT primary key ;
-    user_name varchar(50) not null;
-    user_email varchar(50) not null unique;
+    user_id int AUTO_INCREMENT primary key ,
+    user_name varchar(50) not null,
+    user_email varchar(50) not null unique,
     user_password varchar(60) not null; 
 )
 
@@ -23,18 +23,18 @@ INSERT INTO users (
     ("doron", "doron@gmail.com", "dorondoron");
 
 create table products(
-    product_id int AUTO_INCREMENT primary key ;
-    product_name varchar(50) not null;
-    product_price int not null;
-    product_description varchar(60) not null; 
+    product_id int AUTO_INCREMENT primary key ,
+    product_name varchar(50) not null,
+    product_price int not null,
+    product_description varchar(60) not null,
     product_imgUrl varchar(120) not null;
 )
 create table user_products(
-    user_id int ;
-    product_id int ;
-    product_quantity int not null
+    user_id int ,
+    product_id int ,
+    product_quantity int not null,
     foreign key (user_id) REFERENCES users (user_id),
     foreign key (product_id) REFERENCES products (product_id)
     ON DELETE CASCADE,
-	UNIQUE KEY (product_id, user_id)
+	UNIQUE KEY (product_id, user_id);
 )
