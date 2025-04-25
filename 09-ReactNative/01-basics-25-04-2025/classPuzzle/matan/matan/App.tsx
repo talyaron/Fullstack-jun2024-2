506f559d8c7ee6f8fd9c665e5fa495d501996039
Matan Benoon <matanbn1121@gmail.com>
2025-04-25 12:41:38 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ 09-ReactNative/01-basics-25-04-2025/classPuzzle/matan/matan/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} >Open 33 up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text:{
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  }
+
+
+});
