4fd028b14641d0e031cfce9b7194534d51293215
Elizigi <elizigi876@gmail.com>
2025-04-25 12:25:03 +0300

    update

--- 09-ReactNative/01-basics-25-04-2025/classPuzzle/Eli/App.tsx
+++ 09-ReactNative/01-basics-25-04-2025/classPuzzle/Eli/App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>i did</Text>
+      <Text>reload took 51 ms so long for just text</Text>
+
       <StatusBar style="auto" />
     </View>
   );
