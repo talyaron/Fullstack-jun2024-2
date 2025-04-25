7af936db82be42aabd7b21ab67e64074abac64d1
Elizigi <elizigi876@gmail.com>
2025-04-25 12:29:49 +0300

    up

--- 09-ReactNative/01-basics-25-04-2025/classPuzzle/Eli/App.tsx
+++ 09-ReactNative/01-basics-25-04-2025/classPuzzle/Eli/App.tsx
@@ -4,13 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>i did</Text>
-      <Text>reload took 51 ms so long for just text</Text>
+      <Text style={styles.text}>i did</Text>
+      <Text style={styles.textAngry}>reload took 51 ms so long for just text</Text>
 
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, text: {
+   color:"blue",
+   fontSize:90,
+  }, textAngry: {
+    color:"red",
+    fontSize:190,
+   },
 });
