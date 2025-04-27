8d8a0cb48f55800c6d950f8f25ab53eb584036df
Tal Yaron <tal.yaron@gmail.com>
2025-04-27 20:20:51 +0300

    class puzlle tal

--- 09-ReactNative/02-emulators-27-04-2025/followup/tal/App.tsx
+++ 09-ReactNative/02-emulators-27-04-2025/followup/tal/App.tsx
@@ -1,34 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { textMain } from './assets/colors';
+import WhatsAppMessageCard from './componentes/chatCard/ChatCard';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.blue}></View>
-      <View style={styles.white}></View>
-      <View style={styles.red}></View>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color: textMain }}>WhatsApp</Text>
+        <Text style={{ fontSize: 16 }}>Menu</Text>
+      </View>
+      <View style={styles.content}>
+       <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+        <WhatsAppMessageCard />
+      </View>
+     
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+  },
+  header: {
+    position: 'absolute',
+    top: 15,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: 'white',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    zIndex: 1,
+  },
+  content: {
+    flex: 1,
+    marginTop: 50, // To account for the header height
     alignItems: 'center',
     justifyContent: 'center',
   },
-  blue: {
-    backgroundColor: '#0000FF',
-    flex: 1,
-    width: '100%',
-  },
-  white: {
-    backgroundColor: '#FFFFFF',
-    flex: 1,
-    width: '100%',
-  },
-  red: {
-    backgroundColor: '#FF0000',
-    flex: 1,
-    width: '100%',
-  }
 });
