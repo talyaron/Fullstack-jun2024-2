5665817ca44e7b9a593df3ff79132d7bf7e8b573
Tal Yaron <tal.yaron@gmail.com>
2025-04-27 19:49:11 +0300

    cont

--- 09-ReactNative/02-emulators-27-04-2025/followup/tal/App.tsx
+++ 09-ReactNative/02-emulators-27-04-2025/followup/tal/App.tsx
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.blue}></View>
+      <View style={styles.white}></View>
+      <View style={styles.red}></View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  blue: {
+    backgroundColor: '#0000FF',
+    flex: 1,
+    width: '100%',
+  },
+  white: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    width: '100%',
+  },
+  red: {
+    backgroundColor: '#FF0000',
+    flex: 1,
+    width: '100%',
+  }
 });
