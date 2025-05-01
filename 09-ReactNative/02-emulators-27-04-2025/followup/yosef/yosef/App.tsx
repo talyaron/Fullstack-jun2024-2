b19d0310374ae004f12797ef436b886f3d7941a2
Yosef Ibrahim <yosefib88@gmail.com>
2025-05-01 12:30:23 +0300

    update app

--- 09-ReactNative/02-emulators-27-04-2025/followup/yosef/yosef/App.tsx
+++ 09-ReactNative/02-emulators-27-04-2025/followup/yosef/yosef/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import ChatRoom from "./components/ChatRoom/ChatRoom";
 
@@ -7,9 +7,9 @@ export default function App() {
   return (
     <View>
       <View style={styles.navBarUp}>
-        <Text style={{ left: 0, color:'white' }}>Massenager App</Text>
-        <Text style={{ left: 200 }}>🔎</Text>
-        <Text style={{ left: 230, color: "white" }}>:</Text>
+        <Text style={{ left: 0, color:'white', fontSize:25 }}>Massenager App</Text>
+        <Icon name="search" size={24} color="white" style={{left: 120}}/>
+        <Icon name="more-vert" size={24} color="white" style={{left: 130}}/>
       </View>
       <View style={styles.navBarDown}>
         <Icon name="camera-alt" size={24} color="white" />
@@ -17,9 +17,32 @@ export default function App() {
         <Text style={styles.textWhite}>Status</Text>
         <Text style={styles.textWhite}>Calls</Text>
       </View>
-      <ChatRoom name="Suzy" />
-      <ChatRoom name="Etimad" />
-      <ChatRoom name="yosef" />
+      <FlatList
+        data={[
+          { id: '1', name: 'Suzy', data: '10.2.2025' },
+          { id: '2', name: 'Itemad', data: '15:09' },
+          { id: '3', name: 'Yosef', data: 'yestardy' },
+          { id: '4', name: 'Alex', data: '10.5.2025' },
+          { id: '5', name: 'Chat 5', data: '1.2.2020' },
+          { id: '6', name: 'Chat 6' },
+          { id: '7', name: 'Chat 7' },
+          { id: '8', name: 'Chat 8' },
+          { id: '9', name: 'Chat 9' },
+          { id: '10', name: 'Chat 10' },
+          { id: '11', name: 'Chat 11' },
+          { id: '12', name: 'Chat 12' },
+          { id: '13', name: 'Chat 13' },
+          { id: '14', name: 'Chat 14' },
+          { id: '15', name: 'Chat 15' },
+          { id: '16', name: 'Chat 16' },
+          { id: '17', name: 'Chat 17' },
+          { id: '18', name: 'Chat 18' },
+          { id: '19', name: 'Chat 19' },
+          { id: '20', name: 'Chat 20' },
+        ]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ChatRoom name={item.name} date={item.data} />}
+      />
     </View>
   );
 }
