0cd5972bed481bdaceb2067af8b9b8c279c2e368
Alexis <gog20@icloud.com>
2025-05-06 20:14:10 +0300

    av card

--- 09-ReactNative/04-one-page-project/EliAlexis/App.tsx
+++ 09-ReactNative/04-one-page-project/EliAlexis/App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Day from './components/Day';
-
+import ActivityCard from './components/Card';
 export default function App() {
   return (
     <View >
       <Day />
+      <ActivityCard
+          title="10 min"
+          subtitle="meditation"
+          stars={13}
+          duration="4 h"
+          timerValue="10"
+          backgroundColor="#C89B6C"
+        />
     </View>
   );
 }
