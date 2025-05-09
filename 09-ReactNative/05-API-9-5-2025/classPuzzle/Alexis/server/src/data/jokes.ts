5332e086cd572b950aa6aa62265bb378e9da72f0
Alexis <gog20@icloud.com>
2025-05-09 12:01:56 +0300

    av

---
+++ 09-ReactNative/05-API-9-5-2025/classPuzzle/Alexis/server/src/data/jokes.ts
@@ -0,0 +1,63 @@
+// Array of developer jokes
+export const developerJokes = [
+  {
+    id: 1,
+    setup: "Why do programmers prefer dark mode?",
+    punchline: "Because light attracts bugs!"
+  },
+  {
+    id: 2,
+    setup: "How many programmers does it take to change a light bulb?",
+    punchline: "None, that's a hardware problem."
+  },
+  {
+    id: 3,
+    setup: "Why do Java developers wear glasses?",
+    punchline: "Because they can't C#!"
+  },
+  {
+    id: 4,
+    setup: "Why was the JavaScript developer sad?",
+    punchline: "Because he didn't know how to NULL his feelings."
+  },
+  {
+    id: 5,
+    setup: "What's a programmer's favorite hang out place?",
+    punchline: "The Foo Bar."
+  },
+  {
+    id: 6,
+    setup: "Why do programmers always mix up Halloween and Christmas?",
+    punchline: "Because Oct 31 == Dec 25."
+  },
+  {
+    id: 7, 
+    setup: "Why did the developer go broke?",
+    punchline: "Because he used up all his cache!"
+  },
+  {
+    id: 8,
+    setup: "What do you call a programmer from Finland?",
+    punchline: "Nerdic."
+  },
+  {
+    id: 9,
+    setup: "What's the object-oriented way to become wealthy?",
+    punchline: "Inheritance."
+  },
+  {
+    id: 10,
+    setup: "Why did the functions stop calling each other?",
+    punchline: "They had too many arguments."
+  },
+  {
+    id: 11,
+    setup: "How do you comfort a JavaScript bug?",
+    punchline: "You console it."
+  },
+  {
+    id: 12,
+    setup: "Why did the developer quit his job?",
+    punchline: "Because he didn't get arrays."
+  }
+];
