357158d417103066189c4855c7a94f2f8fc7c10d
Alexis <gog20@icloud.com>
2025-05-09 09:52:43 +0300

    pseudocode cp

---
+++ 09-ReactNative/05-API-9-5-2025/classPuzzle/Alexis/classpuzzle.tsx
@@ -0,0 +1,25 @@
+btn:
+type Props = {
+    onPress: () => void;
+    };
+const btn = ({onPress}: Props)  => {
+    return(
+        <Pressable onPress={onPress}>
+        <Text>▶ Start</Text>
+      </Pressable>
+    );};
+
+
+app:
+const [showpicture, setshowpicture]= usestate(false);
+if(showpicture){
+return(
+  <ImageBackground />
+)}
+else{
+    <View>
+        <FirstPage />
+        <Text>Welcome to the class puzzle</Text>
+        <btn onPress={()=>showpicture(true)}/>
+    </View>
+}
